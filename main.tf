77515f5119b4d0a7c63e3986c473fb3c11d8e381
AblayeT <ablayetoure2014@gmail.com>
2026-01-13 10:28:35 +0100

    Test: Injection de faille SSH (ID: sg-0411ff2f3f047405b)

--- main.tf
+++ main.tf
@@ -3,30 +3,31 @@ provider "aws" {
 }
 
 resource "aws_security_group" "sg_passoire" {
-  name = "sg_vulnerable_ssh"
-  # CORRECTION ICI : On retire les accents
+  name        = "sg_vulnerable_ssh"
   description = "Security Group intentionnellement vulnerable pour Audit"
 
-  # Regle d'entree (Ingress) : C'est ici qu'est la faille !
-  # ingress {
-  #   description = "SSH ouvert au monde entier"
-  #   from_port   = 22
-  #   to_port     = 22
-  #   protocol    = "tcp"
-  #   # 0.0.0.0/0 signifie "Toutes les adresses IP de la Terre"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
-
+  # --- PHASE 1 : VERSION VULNÉRABLE (ACTIVE) ---
+  # On laisse le port 22 ouvert à tout Internet (0.0.0.0/0)
+  # C'est ce bloc qui va déclencher l'alerte Prowler
   ingress {
-    description = "SSH restreint a une adresse IP interne (Admin)"
+    description = "SSH ouvert au monde entier"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    # CORRECTION : On remplace 0.0.0.0/0 (Internet) par une IP précise
-    cidr_blocks = ["10.0.0.5/32"]
+    cidr_blocks = ["0.0.0.0/0"] 
   }
 
-  # Regle de sortie (Egress) : On laisse tout sortir (standard)
+  # --- PHASE 2 : VERSION SÉCURISÉE (COMMENTÉE) ---
+  # On garde ça pour l'étape suivante (la correction)
+  # ingress {
+  #   description = "SSH restreint a une adresse IP interne (Admin)"
+  #   from_port   = 22
+  #   to_port     = 22
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["10.0.0.5/32"]
+  # }
+
+  # Regle de sortie standard
   egress {
     from_port   = 0
     to_port     = 0
